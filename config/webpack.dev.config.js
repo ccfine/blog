ff9fc87a6430d5b42a543044aa91752e65a46ca7
ccfine <469781947@qq.com>
2019-02-07 17:32:32 +0800

    配置webpack.dev

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -6,5 +6,18 @@ module.exports = {
   output: {
     path: path.join(__dirname, "../dist"),
     filename: "js/bundle.[hash].js"
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    host: "localhost",
+    port: 3000,
+    contentBase: path.join(__dirname, "../dist"),
+    historyApiFallback: true,
+    inline: true,
+    open: true,
+    hot: true,
+    overlay: {
+      error: true
+    }
   }
 }
