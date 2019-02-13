bfc0a0c5ccd2d258250be0812f2b4b9425a6844d
ccfine <469781947@qq.com>
2019-02-13 23:08:36 +0800

    配置webpack开发环境

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -1,4 +1,5 @@
 const path = require("path")
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
 
 module.exports = {
   mode: "development",
@@ -27,7 +28,64 @@ module.exports = {
         loader: "babel-loader",
         include: path.join(__dirname, "../src"),
         exclude: path.join(__dirname, "../node_modules")
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [
+            {
+              loader: "css-loader",
+              options: {
+                modules: true,
+                localIdentName: "[name]--[local]__[hash:base64:5]"
+              }
+            },
+            {
+              loader: "postcss-loader",
+              options: {
+                plugins: () => [
+                  require("autoprefixer")
+                ]
+              }
+            }
+          ]
+        }),
+        include: path.join(__dirname, "../src"),
+        exclude: path.join(__dirname, "../node_modules")
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "image/[name].[hash:5].[ext]"
+            }
+          },
+          {
+            loader: "image-webpack-loader"
+          }
+        ],
+        include: path.join(__dirname, "../src"),
+        exclude: path.join(__dirname, "../node_modules")
+      },
+      {
+        test: /\.(eot|svg|ttf|woff)$/,
+        loader: "url-loader",
+        options: {
+          limit: 8192,
+          name: "font/[name].[md5:hash:hex:7],[ext]"
+        },
+        include: path.join(__dirname, "../src"),
+        exclude: path.join(__dirname, "../node_modules")
       }
     ]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: "css/[name].[contenthash:5].css",
+      allChunks: true
+    })
+  ]
 }
