571b595bb93d93b55234bdf9dc405cabfea0dab6
ccfine <469781947@qq.com>
2019-03-11 15:37:40 +0800

    生产环境关闭redux的控制台调试功能，配置基础路由

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import React, { Component } from "react"
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom"
+import Home from "pages/home/Home.jsx"
 
 export default class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      hasError: false
+    }
+  }
+
+  componentDidCatch () {
+    this.setState({
+      hasError: true
+    })
+  }
+
   render () {
-    return (
-      <div>陈景腾的博客</div>
+    return this.state.hasError? (
+      <div>出错啦。。。</div>
+    ): (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/home" exact component={ Home } />
+          <Redirect to="/home" />
+        </Switch>
+      </BrowserRouter>
     )
   }
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,9 +3,15 @@ import thunk from "redux-thunk"
 import { persistStore } from "redux-persist"
 import reducer from "./reducer"
 
-export const store = createStore(reducer, compose(
-  applyMiddleware(thunk),
-  window.devToolsExtension? window.devToolsExtension(): f => f
-))
+export let store
+
+if (process.env.NODE_ENV === "development") {
+  store = createStore(reducer, compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension? window.devToolsExtension(): f => f
+  ))
+} else if (process.env.NODE_ENV === "production") {
+  store = createStore(reducer, applyMiddleware(thunk))
+}
 
 export const persistor = persistStore(store)
