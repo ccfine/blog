f2510ba69123e0acd84d1b8486e049a91c046e8b
ccfine <469781947@qq.com>
2019-06-02 23:34:45 +0800

    npm页面开发

--- src/pages/npm/Npm.jsx
+++ src/pages/npm/Npm.jsx
@@ -12,6 +12,41 @@ export default class Npm extends Component {
       <div>
         <h1>Npm常用指令</h1>
         <Instruction instructionList={ this.props.instructionList } />
+        <h1>yarn与npm</h1>
+        <table border="1">
+          <thead>
+            <tr>
+              <th>yarn</th>
+              <th>npm</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>yarn</td>
+              <td>npm install</td>
+            </tr>
+            <tr>
+              <td>yarn global add</td>
+              <td>npm install -g</td>
+            </tr>
+            <tr>
+              <td>yarn add</td>
+              <td>npm install --save</td>
+            </tr>
+            <tr>
+              <td>yarn add --dev</td>
+              <td>npm install --save-dev</td>
+            </tr>
+            <tr>
+              <td>yarn remove</td>
+              <td>npm uninstall --save(-dev)</td>
+            </tr>
+            <tr>
+              <td>yarn run</td>
+              <td>npm run</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     )
   }
