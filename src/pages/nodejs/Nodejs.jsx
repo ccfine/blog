80a3807e14cff8046224a6011e673da9992ec8ac
ccfine <469781947@qq.com>
2019-06-12 23:49:20 +0800

    nodejs页面开发

--- src/pages/nodejs/Nodejs.jsx
+++ src/pages/nodejs/Nodejs.jsx
@@ -5,6 +5,13 @@ export default class Nodejs extends Component {
     return (
       <div>
         <h1>windows环境下安装nodejs</h1>
+        <div>
+          推荐使用nvm下载并管理nodejs版本，<a href="http://download.ccfine24.com/download/nvm-setup" title="点击下载">下载地址</a>
+        </div>
+        <h2>安装步骤：</h2>
+        <ul>
+          <li>1、下载安装nvm</li>
+        </ul>
       </div>
     )
   }
